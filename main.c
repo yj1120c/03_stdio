6e4a7aa98e48ae9eafa38997ac7fa93092fb1530
yj1120c <70882360+yj1120c@users.noreply.github.com>
2020-09-28 20:10:52 +0900

    04

--- main.c
+++ main.c
@@ -4,14 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char alp;
-	int alpn;
-	printf("enter a character : ");
-	scanf("%c", &alp);
-	
-	alpn = alp;
-	
-	printf("the next charater of %c (%i) is %c (%i)\n", alp, alpn, alp+1, alpn+1);
-	
+	int a, b;
+    float c;
+     
+    printf("분자를 입력하세용 : ");
+    scanf("%d", &a);
+    printf("분모를 입력하세용 : ");
+    scanf("%d", &b);
+    c = (float)a/(float)b;
+    printf("나누기의 결과는 %.6lf 입니다.", c);
+    
 	return 0;
 }
